70306a4144222e02644be3f5f1b2320f991db128
Minyeong Jeong <megormin@gmail.com>
2025-06-21 13:06:29 +0200

    add text

--- src/components/FloorScene.jsx
+++ src/components/FloorScene.jsx
@@ -1,13 +1,15 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { useScroll } from "@react-three/drei";
+import { Billboard, Text, useScroll } from "@react-three/drei";
 import Floor from "./Floor";
 import Arches from "./Arches";
 import Frames from "./Frames";
+import * as THREE from "three";
 import { images } from "../utils/util";
 
 const FloorScene = () => {
   const groupRef = useRef();
+  const textRef = useRef();
   const data = useScroll();
 
   useFrame((state) => {
@@ -16,6 +18,15 @@ const FloorScene = () => {
     const targetZ = offset * -20;
     state.camera.position.z += (targetZ - state.camera.position.z) * 0.1;
     state.camera.lookAt(0, -1, state.camera.position.z - 5);
+
+    if (textRef.current) {
+      const cameraDirection = state.camera.getWorldDirection(
+        new THREE.Vector3()
+      );
+      textRef.current.position
+        .copy(state.camera.position)
+        .add(cameraDirection.multiplyScalar(2)); // 2 units in front of camera
+    }
   });
 
   return (
@@ -23,6 +34,25 @@ const FloorScene = () => {
       <color attach="background" args={["#ffffff"]} />
       <Floor />
       <Arches />
+      <Billboard
+        ref={textRef}
+        follow={true}
+        lockX={false}
+        lockY={false}
+        lockZ={false}
+        scale={0.5}
+      >
+        <Text position-y={0.4} fontSize={0.25} color="black">
+          Selected Photos
+        </Text>
+        <Text position-y={0} fontSize={0.45} color="black">
+          MINYEONG JEONG
+        </Text>
+        <Text position-y={-0.4} fontSize={0.15} color="black">
+          UrbanAesthetic, Minimalism, AnalogVibes
+        </Text>
+      </Billboard>
+
       {images.map((image, index) => {
         const side = index % 2 === 0 ? -1 : 1;
         const x = side * (1.25 + Math.random() * 1.25);
