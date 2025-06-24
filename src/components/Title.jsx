a881f94bb2c57f456ba98331b5b9b49bed51c26f
Minyeong Jeong <megormin@gmail.com>
2025-06-24 19:30:41 +0200

    remove text shaderMaterial

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
-import { extend, useFrame } from "@react-three/fiber";
-import { Billboard, shaderMaterial, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Billboard, Text } from "@react-three/drei";
 import * as THREE from "three";
 import { fontUrl } from "../utils/util";
 
@@ -18,43 +18,30 @@ const Title = () => {
     }
   });
 
-  const TextMaterial = shaderMaterial(
-    {
-      uTime: 0,
-      uColor: new THREE.Color(0x000000),
-    },
-    `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `,
-    `
-      uniform float uTime;
-      uniform vec3 uColor;
-      varying vec2 vUv;
-      
-      void main() {
-        gl_FragColor = vec4(uColor, 1.0);
-      }
-    `
-  );
-
-  extend({ TextMaterial });
-
   return (
     <Billboard ref={textRef} scale={0.5}>
-      <Text position-y={0.6} fontSize={0.35} font={fontUrl.averia}>
-        <textMaterial />
+      <Text
+        position-y={0.6}
+        fontSize={0.35}
+        font={fontUrl.averia}
+        color={"#000000"}
+      >
         Selected Photos by
       </Text>
-      <Text position-y={0} fontSize={0.6} font={fontUrl.bebas}>
-        <textMaterial />
+      <Text
+        position-y={0}
+        fontSize={0.6}
+        font={fontUrl.bebas}
+        color={"#000000"}
+      >
         MINYEONG JEONG
       </Text>
-      <Text position-y={-0.5} fontSize={0.2} font={fontUrl.montserrat}>
-        <textMaterial />
+      <Text
+        position-y={-0.5}
+        fontSize={0.2}
+        font={fontUrl.montserrat}
+        color={"#000000"}
+      >
         Urban Aesthetic
       </Text>
     </Billboard>
